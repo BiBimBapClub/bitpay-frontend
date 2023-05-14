7834873de6b8cf1868d723ee01bc94e95390484d
Jeon <gino9940@gmail.com>
2023-05-15 01:24:46 +0900

    feat: add postOrder example

---
+++ src/Shared/apis/postOrder.js
@@ -0,0 +1,14 @@
+import instance from "./index";
+
+const postOrder = async () => {
+  const data = await instance.post("/orders", {
+    tableId: 13,
+    orderDetail: [
+      { menu_id: 1, count: 3 },
+      { menu_id: 2, count: 3 },
+    ],
+  });
+  return data;
+};
+
+export default postOrder;
