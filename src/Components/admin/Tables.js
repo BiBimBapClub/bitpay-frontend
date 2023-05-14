de13e7b3c2c8e56f53337c0f03e8c4d9399de26b
binllionaire <bug20474@gmail.com>
2023-05-15 08:17:57 +0900

    update

--- src/Components/admin/Tables.js
+++ src/Components/admin/Tables.js
@@ -1,84 +1,151 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate,useLocation } from "react-router-dom";
-import {getTables,postCleanFinish,postOut,postIn} from "../../Shared/apis/getTables";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import {
+  getTables,
+  postCleanFinish,
+  postOut,
+  postIn,
+} from "../../Shared/apis/getTables";
 
 const Table = () => {
-  const navigate = useNavigate();
-  const navigateToDetail = (index) => {
-    navigate("/admin/"+index)
-  };
-  const [tableData, setTableData] = useState([]
-  );
+  const [tableData, setTableData] = useState([]);
+  
   const fetchData = async () => {
     const tables = await getTables();
     setTableData(tables.data);
-    console.log(tables)
+    console.log(tables);
   };
+
   useEffect(() => {
-    fetchData()
+    fetchData();
     const interval = setInterval(fetchData, 5000);
     return () => clearInterval(interval);
   }, []);
-  
+
+
+  const oddRows = tableData?.filter((row, index) => index % 2 === 0);
+  const evenRows = tableData?.filter((row, index) => index % 2 === 1);
+
+  return (
+    <div className="flex">
+      <table className="w-1/2 mr-4">
+        <tbody>
+          {oddRows?.map((row) => (
+            <TableItem key={row.nunber} table={row} setTableData={setTableData}/>
+          ))}
+        </tbody>
+      </table>
+      <table className="w-1/2 mr-4">
+        <tbody>
+          {evenRows?.map((row) => (
+            <TableItem key={row.nunber} table={row} setTableData={setTableData}/>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const TableItem = ({ table,setTableData }) => {
+  const [timer, setTimer] = useState({
+    hours: 0,
+    minutes: 0,
+  });
+  const navigate = useNavigate();
+  const navigateToDetail = (index) => {
+    navigate("/admin/" + index);
+  };
+  const fetchData = async () => {
+    const tables = await getTables();
+    setTableData(tables.data);
+    console.log(tables);
+  };
   const handleCleaned = (index) => {
-    let num = index.split(":")[1]
-    console.log(num)
-   postCleanFinish(num)
-   fetchData()
+    let num = index.split(":")[1];
+    console.log(num);
+    postCleanFinish(num);
+    fetchData();
   };
   const handleOut = (index) => {
-    let num = index.split(":")[1]
-    console.log(num)
-    postOut(num)
-    fetchData()
-   };
-   const handleIn = (index) => {
-    let num = index.split(":")[1]
-    console.log(num)
-    postIn(num)
-    fetchData()
-   };
-  const handleStatusChange = (index, newStatus) => {
-    setTableData((prevData) => {
-      const newData = [...prevData];
-      newData[index].status = newStatus;
-
-      return newData;
-    });
+    let num = index.split(":")[1];
+    console.log(num);
+    postOut(num);
+    fetchData();
   };
-  
+  const handleIn = (index) => {
+    let num = index.split(":")[1];
+    console.log(num);
+    postIn(num);
+    fetchData();
+  };
+
+  const startTime = new Date(table?.updatedTime);
+  startTime.setHours(startTime.getHours() + 2);
+
+  useEffect(() => {
+    const countdown = setInterval(() => {
+      var now = new Date();
+
+      var differenceInMilliseconds = startTime.getTime() - now.getTime();
+
+      var differenceInMinutes = Math.floor(
+        differenceInMilliseconds / (1000 * 60)
+      );
+      var differenceInHours = Math.floor(differenceInMinutes / 60);
+      var remainingMinutes = differenceInMinutes % 60;
+
+      setTimer({
+        hours: differenceInHours,
+        minutes: remainingMinutes,
+      });
+    }, 1000);
+
+    if (timer.hours < 0 || timer.minutes < 0) {
+      clearInterval(countdown);
+    }
+
+    return () => clearInterval(countdown);
+  }, [startTime, timer.hours, timer.minutes]);
 
   const renderStatusRow = (status, index, time) => {
-    console.log(status)
-    if (status === '청소요청') {
+    if (status === "청소요청") {
       return (
         <>
           <td className="border pl-4 py-2 bg-red-500 text-white">치워주세요</td>
           <td className="border pl-4 py-2">
-            <button className="bg-slate-200 w-2/3 border-2 border-solid border-black" onClick={() => handleCleaned(index)}>
+            <button
+              className="bg-slate-200 w-2/3 border-2 border-solid border-black"
+              onClick={() => handleCleaned(index)}
+            >
               완료
             </button>
           </td>
         </>
       );
-    } else if (status === '사용중') {
-     
+    } else if (status === "사용중") {
       return (
         <>
-          <td className="border pl-4 py-2">{time}</td>
+          <td className="border pl-4 py-2">{timer.hours.toString().padStart(2, "0")}:
+        {timer.minutes.toString().padStart(2, "0")}</td>
           <td className="border pl-4 py-2">
-            <button className="bg-slate-200 w-2/3 border-2 border-solid border-black" onClick={() => handleOut(index)}>
+            <button
+              className="bg-slate-200 w-2/3 border-2 border-solid border-black"
+              onClick={() => handleOut(index)}
+            >
               퇴장
             </button>
           </td>
         </>
       );
-    } else if (status === '청소완료') {
+    } else if (status === "청소완료") {
       return (
         <>
           <td className="bg-blue-500 text-white border pl-4 py-2">클린</td>
           <td className="border pl-4 py-2">
-            <button className="bg-slate-200 w-2/3 border-2 border-solid border-black" onClick={() => handleIn(index)}>
+            <button
+              className="bg-slate-200 w-2/3 border-2 border-solid border-black"
+              onClick={() => handleIn(index)}
+            >
               입장
             </button>
           </td>
@@ -89,34 +156,17 @@ const Table = () => {
     }
   };
 
-  const oddRows = tableData?.filter((row, index) => index % 2 === 0);
-  const evenRows = tableData?.filter((row, index) => index % 2 === 1);
-
   return (
-    <div className="flex">
-      <table className="w-1/2 mr-4">
-        <tbody>
-          {oddRows?.map((row, index) => (
-            <tr key={index} className="bg-gray-100" >
-              <td className="border px-4 py-2 cursor-pointer " onClick={() => navigateToDetail(row.number.split(":")[1])}>{row.number.split(":")[1]}</td>
-              {renderStatusRow(row.status, row.number, row.updatedTime)}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <table className="w-1/2 mr-4">
-        <tbody>
-          {evenRows?.map((row, index) => (
-            <tr key={index} className="bg-gray-100"  >
-              <td className="border px-4 py-2 cursor-pointer" onClick={() => navigateToDetail(row.number.split(":")[1])} >{row.number.split(":")[1]}</td>
-              {renderStatusRow(row.status, row.number, row.updatedTime)}
-            </tr>
-          ))}
- </tbody>
-      </table>
-    </div>
+    <tr className="bg-gray-100">
+      <td
+        className="border px-4 py-2 cursor-pointer"
+        onClick={() => navigateToDetail(table.number.split(":")[1])}
+      >
+        {table.number.split(":")[1]}
+      </td>
+      {renderStatusRow(table.status, table.number, table.updatedTime)}
+    </tr>
   );
 };
-
 
 export default Table;
--- src/Components/pos/PaymentRequest.js
+++ src/Components/pos/PaymentRequest.js
@@ -1,91 +1,108 @@
-import React, { useState,useEffect } from "react";
-import axios from 'axios';
-import {getOrders,getPaymentRequest,getServeRequest,confirmPayment,confirmServe,cancelPayment} from "../../Shared/apis/getOrders";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  getOrders,
+  getPaymentRequest,
+  getServeRequest,
+  confirmPayment,
+  confirmServe,
+  cancelPayment,
+} from "../../Shared/apis/getOrders";
+import { getAdmin } from "../../Shared/apis/getAdmin";
+import Menus from '../../Menus.json'
 
 export default function PaymentRequest() {
-   const [orderItems, setOrderItems] = useState([]);
-    const [serveItems, setServeItems] = useState([]);
-    useEffect(() => {
-      const fetchData = async () => {
-        const payment = await getPaymentRequest();
-        const serve = await getServeRequest();
-        console.log(payment);
-        setOrderItems(payment.data);
-        setServeItems(serve.data);
-        console.log(orderItems);
-        console.log(serveItems);
-      };
-    
-      const interval = setInterval(fetchData, 5000);
-    
-      return () => clearInterval(interval);
-    }, []);
-   
-  
-    const handleConfirm = (item) => {
-      //setOrderItems(orderItems.filter((order) => order !== item));
-      //setServeItems([...serveItems, item]);
-      confirmPayment(item.id);
+  const [orderItems, setOrderItems] = useState([]);
+  const [serveItems, setServeItems] = useState([]);
 
-    };
-    const handleCancel = (item) => {
-      cancelPayment(item.id)
-    };
-    const handleServeComplete = (item) => {
-        //setServeItems(serveItems.filter((serve) => serve !== item));
-        confirmServe(item.id)
-        console.log(serveItems)
-      };
-    
-    return (
-        <div className="flex">
-          <div className="w-1/2 mt-3">
-            {orderItems?.map((item) => (
-              <div key={item.id} className="overflow-auto w-full h-fit text-black text-xl font-bold text-center mb-3 ">
-                <div className="w-1/5 float-left">{item.id}</div>
-                <div className="w-1/5 float-left">{item.tableNumber}</div>
-                <div className="w-1/5 float-left">{/*item.orderMenu*/}</div>
-                <div className="w-1/5 float-left">{item.totalPrice}</div>
-                <div className="w-1/5 float-left">
-                  {!item.paymentConfirmed && (
-                    <button
-                      className="bg-slate-200 w-2/3 border-2 border-solid border-black"
-                      onClick={() => handleConfirm(item)}
-                    >
-                      확인
-                    </button>
-                  )}
-                  <button
-                      className="bg-slate-200 w-2/3 border-2 border-solid border-black mt-1 text-red-600"
-                      onClick={() => handleCancel(item)}
-                    >
-                      거절
-                    </button>
-                </div>
-              </div>
-            ))}
+  // const orderedMenus = Menus.filter((menu) => menu.id === detailList[0].menuId);
+  console.log(orderItems)
+
+  const fetchData = async () => {
+    const payment = await getPaymentRequest();
+    const serve = await getServeRequest();
+
+    setOrderItems(payment.data.reverse());
+    setServeItems(serve.data.reverse());
+  };
+
+  useEffect(() => {
+    fetchData();
+
+    const interval = setInterval(fetchData, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleConfirm = (item) => {
+    //setOrderItems(orderItems.filter((order) => order !== item));
+    //setServeItems([...serveItems, item]);
+    confirmPayment(item.id);
+  };
+  const handleCancel = (item) => {
+    cancelPayment(item.id);
+  };
+  const handleServeComplete = (item) => {
+    //setServeItems(serveItems.filter((serve) => serve !== item));
+    confirmServe(item.id);
+    console.log(serveItems);
+  };
+
+  return (
+    <div className="flex">
+      <div className="w-1/2 mt-3">
+        {orderItems?.map((item) => (
+          <div
+            key={item.id}
+            className="overflow-auto w-full h-fit text-black text-xl font-bold text-center mb-3 "
+          >
+            <div className="w-1/5 float-left">{item.id}</div>
+            <div className="w-1/5 float-left">{item.tableNumber}</div>
+             <div className="w-1/5 float-left">{item.detailList.map(menu => <span>{Menus.find(m => m.id === menu.menuId).name} * {menu.quantity}개 <br/><br/></span>)}</div>
+            <div className="w-1/5 float-left">{item.totalPrice}</div>
+            <div className="w-1/5 float-left">
+              {!item.paymentConfirmed && (
+                <button
+                  className="bg-slate-200 w-2/3 border-2 border-solid border-black"
+                  onClick={() => handleConfirm(item)}
+                >
+                  확인
+                </button>
+              )}
+              <button
+                className="bg-slate-200 w-2/3 border-2 border-solid border-black mt-1 text-red-600"
+                onClick={() => handleCancel(item)}
+              >
+                거절
+              </button>
+            </div>
           </div>
-    
-          <div className="w-1/2 mt-3">
-            {serveItems?.map((item) => (
-              <div key={item.id} className="overflow-auto w-full text-black text-xl font-bold text-center mb-3">
-                <div className="w-1/5 float-left">{item.id}</div>
-                <div className="w-1/5 float-left">{item.tableNumber}</div>
-                <div className="w-1/5 float-left">{/*item.orderMenu*/}</div>
-                <div className="w-1/5 float-left">{item.totalPrice}</div>
-                <div className="w-1/5 float-left">
-                    {!item.paymentConfirmed && (
-                    <button
-                    className="bg-slate-200 w-2/3 border-2 border-solid border-black"
-                    onClick={() => handleServeComplete(item)}
-                    >
-                    서빙완료
-                    </button>
-                )}
-                </div>
-              </div>
-            ))}
+        ))}
+      </div>
+
+      <div className="w-1/2 mt-3">
+        {serveItems?.map((item) => (
+          <div
+            key={item.id}
+            className="overflow-auto w-full text-black text-xl font-bold text-center mb-3"
+          >
+            <div className="w-1/5 float-left">{item.id}</div>
+            <div className="w-1/5 float-left">{item.tableNumber}</div>
+            <div className="w-1/5 float-left">{item.detailList.map(menu => <span>{Menus.find(m => m.id === menu.menuId).name} * {menu.quantity}개 <br/><br/></span>)}</div>
+            <div className="w-1/5 float-left">{item.totalPrice}</div>
+            <div className="w-1/5 float-left">
+              {!item.paymentConfirmed && (
+                <button
+                  className="bg-slate-200 w-2/3 border-2 border-solid border-black"
+                  onClick={() => handleServeComplete(item)}
+                >
+                  서빙완료
+                </button>
+              )}
+            </div>
           </div>
-        </div>
-      );
-  }
+        ))}
+      </div>
+    </div>
+  );
+}
