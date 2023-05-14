c2db04c9c9e0d79f1250305a8783ce69f72f699d
Jeon <gino9940@gmail.com>
2023-05-15 04:45:35 +0900

    fix: remove placeholder

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -113,7 +113,6 @@ export default function Home() {
             <input
               type="text"
               name="search"
-              placeholder={queryData.tableId}
               className="peer block bg-white w-full border border-slate-300 rounded-md py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
               vaue={tableIdInput}
               onChange={(e) => setTableIdInput(e.target.value)}
