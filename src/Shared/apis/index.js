0569fb00bd912f382aefe0c587d6357286de8c26
Jeon <gino9940@gmail.com>
2023-05-15 05:24:36 +0900

    fix: baseURL

--- src/Shared/apis/index.js
+++ src/Shared/apis/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://117.16.137.229:8080/", // TBD: "https://fest-api.bibim-bap.com/"
+  baseURL: "http://fest-api.bibim-bap.com/",
 });
 
 export default instance;
