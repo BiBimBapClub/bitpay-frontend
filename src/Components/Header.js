0f12943d4419709bdf64fb24d473403103b4bc42
Jeon <gino9940@gmail.com>
2023-05-14 23:30:31 +0900

    fix: remove path parameter

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,10 @@
 import React from "react";
-import { useParams } from "react-router-dom";
 
 export default function Header() {
-  const params = useParams()
-
   return (
     <div className="bg-mainOrange h-fit p-5 flex justify-between items-center">
       <div className="bg-white text-sm rounded-3xl px-4 py-2">00:00</div>
-      <div className="text-white text-2xl font-bold">테이블 {params.tableId}</div>
+      <div className="text-white text-2xl font-bold">테이블 {13}</div>
       <a href="order">
         <div className="bg-white text-sm rounded-3xl px-4 py-2 cursor-pointer">
           주문 내역
