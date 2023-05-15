6613cad4d53758e51dc9c7e164c00dcf31c13f51
Yoo TaeSeung <remicon99@naver.com>
2023-05-15 16:56:29 +0900

    남은 수량 수정중..

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -42,9 +42,7 @@ export default function Nav({ setSelectedMenu }) {
 
   useEffect(() => {
     fetchData();
-  }, []);
 
-  useEffect(() => {
     const setMenu = [];
     const singleMenu = [];
     const service = [];
