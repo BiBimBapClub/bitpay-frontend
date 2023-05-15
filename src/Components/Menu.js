1c9a7111de30c9659712af04d769f9b7c1862e90
Jeon <gino9940@gmail.com>
2023-05-15 19:22:42 +0900

    fix: menu item style

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -2,7 +2,7 @@ import React from "react";
 export default function Menu({ src, name, price, setSelectedMenu, count }) {
   return (
     <div
-      className="justify-center items-center p-5 w-full cursor-pointer"
+      className="justify-center items-center p-2 w-full cursor-pointer"
       onClick={() => {
         setSelectedMenu(name);
       }}
@@ -12,7 +12,7 @@ export default function Menu({ src, name, price, setSelectedMenu, count }) {
         alt="menu1"
         className="h-1/1 w-1/1 rounded-md mb-2"
       />
-      <div className="text-lg font-medium mb-1">{name}</div>
+      <div className="text-base font-medium mb-1 leading-6">{name}</div>
       <div className="text-sm text-gray-500">
         {price.toLocaleString()}원 {count !== 9999 && ` · 남은수량 ${count}개`}
       </div>
