28b41db292c0a3cb421befc89f0418d1f7152347
Jeon <gino9940@gmail.com>
2023-05-15 21:02:50 +0900

    fix: minor style

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -13,15 +13,15 @@ export default function Menu({ src, name, price, setSelectedMenu, count }) {
         className="h-1/1 w-1/1 rounded-md mb-2"
       />
       <div className="text-base font-medium mb-1 leading-6">{name}</div>
-      <div className="text-sm text-gray-500">
+      <span className="text-sm text-gray-500">
         {price.toLocaleString()}원{" "}
         {count < 10 && (
-          <div className="text-sm text-red-500 font-bold">
+          <span className="text-sm text-red-500 font-bold">
             {" "}
-            · 남은수량 {count}개
-          </div>
+            남은수량 {count}개
+          </span>
         )}
-      </div>
+      </span>
     </div>
   );
 }
