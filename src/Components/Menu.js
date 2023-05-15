282a898d56304c53a8b4466b45d77e43046e2563
Jeon <gino9940@gmail.com>
2023-05-15 19:03:50 +0900

    fix: menu item design

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import { getMenus } from "../Shared/apis/getMenus";
+import React from "react";
 export default function Menu({ src, name, price, setSelectedMenu, count }) {
   return (
     <div
@@ -11,13 +10,12 @@ export default function Menu({ src, name, price, setSelectedMenu, count }) {
       <img
         src={`${process.env.PUBLIC_URL + src}`}
         alt="menu1"
-        className="h-1/1 w-1/1"
+        className="h-1/1 w-1/1 rounded-md mb-2"
       />
-      <div className="text-lg text-center">{name}</div>
-      <div className="text-sm text-center">{price.toLocaleString()}원</div>
-      {count != 9999 ? (
-        <div className="text-xs text-center pt-1">남은수량 : {count}개</div>
-      ) : null}
+      <div className="text-lg font-medium mb-1">{name}</div>
+      <div className="text-sm text-gray-500">
+        {price.toLocaleString()}원 {count !== 9999 && ` · 남은수량 ${count}개`}
+      </div>
     </div>
   );
 }
