4ff512e36b41ae82320e9470c4128e6ebe6cc220
Jeon <gino9940@gmail.com>
2023-05-15 19:41:06 +0900

    fix: sheet item layout

--- src/Components/SheetItem.js
+++ src/Components/SheetItem.js
@@ -42,40 +42,43 @@ export default function SheetItem({
 
   return (
     <div className="flex justify-center items-center my-3 px-3">
-      <div className="w-1/2 flex items-center">
+      <div className="w-full flex items-center">
         <img
           src={`${process.env.PUBLIC_URL + menu.src}`}
           alt="menu1"
-          className="h-20 w-20"
+          className="h-20 w-20 rounded-md border-2 border-slate-200"
         />
         <div className="ml-3">
-          <div className="text-base">{menu.name}</div>
-          <div className="text-sm">{menu.price.toLocaleString()}원</div>
+          <div className="text-base font-medium">{menu.name}</div>
+          <div className="text-sm text-gray-500">
+            {menu.price.toLocaleString()}원
+          </div>
         </div>
       </div>
 
-      <div className="w-1/4 flex justify-start items-center">
-        <button
-          className="border bg-mainOrange text-white p-1 w-6"
-          onClick={onClickMinusBtn}
-        >
-          {menu.count > 1 ? (
-            <FontAwesomeIcon icon={faMinus} />
-          ) : (
-            <FontAwesomeIcon icon={faTrashCan} />
-          )}
-        </button>
-        <div className="border w-6 py-1 mx-1 text-center">{menu.count}</div>
-        <button
-          className="border w-6 bg-mainOrange text-white p-1"
-          onClick={onClickPlusBtn}
-        >
-          <FontAwesomeIcon icon={faPlus} />
-        </button>
-      </div>
-
-      <div className="text-base w-1/4 flex justify-end">
-        ₩ {(menu.count * menu.price).toLocaleString()}원
+      <div className="flex flex-col w-1/4 justify-center aligns-center ml-2">
+        <div className="text-sm flex text-gray-500 mb-1">
+          ₩ {(menu.count * menu.price).toLocaleString()}원
+        </div>
+        <div className="flex justify-start items-center">
+          <button
+            className="border bg-mainOrange text-white p-1 w-6"
+            onClick={onClickMinusBtn}
+          >
+            {menu.count > 1 ? (
+              <FontAwesomeIcon icon={faMinus} />
+            ) : (
+              <FontAwesomeIcon icon={faTrashCan} />
+            )}
+          </button>
+          <div className="border w-6 py-1 mx-1 text-center">{menu.count}</div>
+          <button
+            className="border w-6 bg-mainOrange text-white p-1"
+            onClick={onClickPlusBtn}
+          >
+            <FontAwesomeIcon icon={faPlus} />
+          </button>
+        </div>
       </div>
     </div>
   );
